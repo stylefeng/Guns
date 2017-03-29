e01a48ffb0282a62fc3ed7f1814ad9a224cf5b3c
fsn <sn93@qq.com>
2017-03-29 19:53:04 +0800

    整理

--- src/main/java/com/stylefeng/guns/common/page/PageBT.java
+++ src/main/java/com/stylefeng/guns/common/page/PageBT.java
@@ -1,7 +1,6 @@
 package com.stylefeng.guns.common.page;
 
 /**
- * 
  * 分页参数类（for BootStrap Table）
  *
  * @author fengshuonan
@@ -9,58 +8,58 @@ package com.stylefeng.guns.common.page;
  */
 public class PageBT {
 
-	private int limit; 		// 每页显示个数
+    private int limit;        // 每页显示个数
 
-	private int offset; 	// 查询的偏移量（查询的页数 = offset/limit + 1）
+    private int offset;    // 查询的偏移量（查询的页数 = offset/limit + 1）
 
-	private String order; 	// 排序方式
-	
-	
-	public PageBT() {
-		super();
-	}
+    private String order;    // 排序方式
 
-	public PageBT(int limit, int offset) {
-		super();
-		this.limit = limit;
-		this.offset = offset;
-	}
 
-	public int getLimit() {
-		return limit;
-	}
+    public PageBT() {
+        super();
+    }
 
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
+    public PageBT(int limit, int offset) {
+        super();
+        this.limit = limit;
+        this.offset = offset;
+    }
 
-	public int getOffset() {
-		return offset;
-	}
+    public int getLimit() {
+        return limit;
+    }
 
-	public void setOffset(int offset) {
-		this.offset = offset;
-	}
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 
-	public String getOrder() {
-		return order;
-	}
+    public int getOffset() {
+        return offset;
+    }
 
-	public void setOrder(String order) {
-		this.order = order;
-	}
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 
-	public int getPageSize() {
-		return this.limit;
-	}
+    public String getOrder() {
+        return order;
+    }
 
-	public int getPageNumber() {
-		return this.offset / this.limit + 1;
-	}
+    public void setOrder(String order) {
+        this.order = order;
+    }
 
-	@Override
-	public String toString() {
-		return "PageBT [limit=" + limit + ", offset=" + offset + ", order=" + order + "]";
-	}
+    public int getPageSize() {
+        return this.limit;
+    }
+
+    public int getPageNumber() {
+        return this.offset / this.limit + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "PageBT [limit=" + limit + ", offset=" + offset + ", order=" + order + "]";
+    }
 
 }
