0ae7c1884fa0bfaf0bfb882c22f76d87ca06476e
fsn <sn93@qq.com>
2017-04-08 11:18:46 +0800

    增加对菜单的排序

--- src/main/java/com/stylefeng/guns/common/node/MenuNode.java
+++ src/main/java/com/stylefeng/guns/common/node/MenuNode.java
@@ -1,243 +1,289 @@
 package com.stylefeng.guns.common.node;
 
+import com.stylefeng.guns.core.util.ToolUtil;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import com.stylefeng.guns.core.util.ToolUtil;
-
 /**
- * @Description 菜单的节点
  * @author fengshuonan
+ * @Description 菜单的节点
  * @date 2016年12月6日 上午11:34:17
  */
-public class MenuNode {
+public class MenuNode implements Comparable {
 
-	/**
-	 * 节点id
-	 */
-	private Integer id;
+    /**
+     * 节点id
+     */
+    private Integer id;
 
-	/**
-	 * 父节点
-	 */
-	private Integer parentId;
+    /**
+     * 父节点
+     */
+    private Integer parentId;
 
-	/**
-	 * 节点名称
-	 */
-	private String name;
-	
-	/**
-	 * 按钮级别
-	 */
-	private Integer levels;
+    /**
+     * 节点名称
+     */
+    private String name;
 
-	/**
-	 * 节点的url
-	 */
-	private String url;
+    /**
+     * 按钮级别
+     */
+    private Integer levels;
 
-	/**
-	 * 节点图标
-	 */
-	private String icon;
+    /**
+     * 按钮的排序
+     */
+    private Integer num;
 
-	/**
-	 * 子节点的集合
-	 */
-	private List<MenuNode> children;
+    /**
+     * 节点的url
+     */
+    private String url;
 
-	/**
-	 * 查询子节点时候的临时集合
-	 */
-	private List<MenuNode> linkedList = new ArrayList<MenuNode>();
+    /**
+     * 节点图标
+     */
+    private String icon;
 
-	public MenuNode() {
-		super();
-	}
+    /**
+     * 子节点的集合
+     */
+    private List<MenuNode> children;
 
-	public MenuNode(Integer id, Integer parentId) {
-		super();
-		this.id = id;
-		this.parentId = parentId;
-	}
-	
-	public Integer getLevels() {
-		return levels;
-	}
+    /**
+     * 查询子节点时候的临时集合
+     */
+    private List<MenuNode> linkedList = new ArrayList<MenuNode>();
 
-	public void setLevels(Integer levels) {
-		this.levels = levels;
-	}
+    public MenuNode() {
+        super();
+    }
 
-	public String getIcon() {
-		return icon;
-	}
+    public MenuNode(Integer id, Integer parentId) {
+        super();
+        this.id = id;
+        this.parentId = parentId;
+    }
 
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
+    public Integer getLevels() {
+        return levels;
+    }
 
-	public static MenuNode createRoot() {
-		return new MenuNode(0, -1);
-	}
+    public void setLevels(Integer levels) {
+        this.levels = levels;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public String getIcon() {
+        return icon;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
 
-	public Integer getParentId() {
-		return parentId;
-	}
+    public static MenuNode createRoot() {
+        return new MenuNode(0, -1);
+    }
 
-	public void setParentId(Integer parentId) {
-		this.parentId = parentId;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Integer getParentId() {
+        return parentId;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public List<MenuNode> getChildren() {
-		return children;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setChildren(List<MenuNode> children) {
-		this.children = children;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	/**
-	 * @Description 构建整个菜单树
-	 * @param list
-	 *            需要被构建的节点集合
-	 * @author fengshuonan
-	 */
-	public void buildNodeTree(List<MenuNode> nodeList) {
-		for (MenuNode treeNode : nodeList) {
-			List<MenuNode> linkedList = treeNode.findChildNodes(nodeList, treeNode.getId());
-			if (linkedList.size() > 0) {
-				treeNode.setChildren(linkedList);
-			}
-		}
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	/**
-	 * @Description 查询子节点的集合
-	 * @author fengshuonan
-	 */
-	public List<MenuNode> findChildNodes(List<MenuNode> nodeList, Integer parentId) {
-		if (nodeList == null && parentId == null)
-			return null;
-		for (Iterator<MenuNode> iterator = nodeList.iterator(); iterator.hasNext();) {
-			MenuNode node = (MenuNode) iterator.next();
-			// 根据传入的某个父节点ID,遍历该父节点的所有子节点
-			if (node.getParentId() != 0 && parentId == node.getParentId()) {
-				recursionFn(nodeList, node, parentId);
-			}
-		}
-		return linkedList;
-	}
+    public List<MenuNode> getChildren() {
+        return children;
+    }
 
-	/**
-	 * @Description 遍历一个节点的子节点
-	 * @author fengshuonan
-	 */
-	public void recursionFn(List<MenuNode> nodeList, MenuNode node, Integer pId) {
-		List<MenuNode> childList = getChildList(nodeList, node);// 得到子节点列表
-		if (childList.size() > 0) {// 判断是否有子节点
-			if (node.getParentId() == pId) {
-				linkedList.add(node);
-			}
-			Iterator<MenuNode> it = childList.iterator();
-			while (it.hasNext()) {
-				MenuNode n = (MenuNode) it.next();
-				recursionFn(nodeList, n, pId);
-			}
-		} else {
-			if (node.getParentId() == pId) {
-				linkedList.add(node);
-			}
-		}
-	}
+    public void setChildren(List<MenuNode> children) {
+        this.children = children;
+    }
 
-	/**
-	 * @Description 得到子节点列表
-	 * @author fengshuonan
-	 */
-	private List<MenuNode> getChildList(List<MenuNode> list, MenuNode node) {
-		List<MenuNode> nodeList = new ArrayList<MenuNode>();
-		Iterator<MenuNode> it = list.iterator();
-		while (it.hasNext()) {
-			MenuNode n = (MenuNode) it.next();
-			if (n.getParentId() == node.getId()) {
-				nodeList.add(n);
-			}
-		}
-		return nodeList;
-	}
+    public Integer getNum() {
+        return num;
+    }
 
-	@Override
-	public String toString() {
-		return "MenuNode [id=" + id + ", parentId=" + parentId + ", name=" + name + ", url=" + url + ", children="
-				+ children + "]";
-	}
-	
-	/**
-	 * 清除掉按钮级别的资源
-	 * @param nodes
-	 * @return
-	 * @date 2017年2月19日 下午11:04:11
-	 */
-	public static List<MenuNode> clearBtn(List<MenuNode> nodes){
-		ArrayList<MenuNode> noBtns = new ArrayList<MenuNode>();
-		for(MenuNode node : nodes){
-			if(node.getLevels() < 3){
-				noBtns.add(node);
-			}
-		}
-		return noBtns;
-	}
-	
-	/**
-	 * 清除不包含子节点的节点
-	 * @return
-	 * @date 2017年2月19日 下午11:18:19
-	 */
-	public static List<MenuNode> clearNoChild(List<MenuNode> nodes){
-		ArrayList<MenuNode> results = new ArrayList<MenuNode>();
-		for(MenuNode node : nodes){
-			if(ToolUtil.isNotEmpty(node.getChildren())){
-				results.add(node);
-			}
-		}
-		return results;
-	}
-	
-	/**
-	 * 构建菜单列表
-	 * @return
-	 * @date 2017年2月19日 下午11:18:19
-	 */
-	public static List<MenuNode> buildTitle(List<MenuNode> nodes){
-		List<MenuNode> clearBtn = clearBtn(nodes);
-		new MenuNode().buildNodeTree(clearBtn);
-		return clearNoChild(clearBtn);
-	}
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuNode{" +
+                "id=" + id +
+                ", parentId=" + parentId +
+                ", name='" + name + '\'' +
+                ", levels=" + levels +
+                ", num=" + num +
+                ", url='" + url + '\'' +
+                ", icon='" + icon + '\'' +
+                ", children=" + children +
+                ", linkedList=" + linkedList +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        MenuNode menuNode = (MenuNode) o;
+        Integer num = menuNode.getNum();
+        if (num == null) {
+            num = 0;
+        }
+        return this.num.compareTo(num);
+    }
+
+    /**
+     * 构建整个菜单树
+     *
+     * @author fengshuonan
+     */
+    public void buildNodeTree(List<MenuNode> nodeList) {
+        for (MenuNode treeNode : nodeList) {
+            List<MenuNode> linkedList = treeNode.findChildNodes(nodeList, treeNode.getId());
+            if (linkedList.size() > 0) {
+                treeNode.setChildren(linkedList);
+            }
+        }
+    }
+
+    /**
+     * 查询子节点的集合
+     *
+     * @author fengshuonan
+     */
+    public List<MenuNode> findChildNodes(List<MenuNode> nodeList, Integer parentId) {
+        if (nodeList == null && parentId == null)
+            return null;
+        for (Iterator<MenuNode> iterator = nodeList.iterator(); iterator.hasNext(); ) {
+            MenuNode node = (MenuNode) iterator.next();
+            // 根据传入的某个父节点ID,遍历该父节点的所有子节点
+            if (node.getParentId() != 0 && parentId == node.getParentId()) {
+                recursionFn(nodeList, node, parentId);
+            }
+        }
+        return linkedList;
+    }
+
+    /**
+     * 遍历一个节点的子节点
+     *
+     * @author fengshuonan
+     */
+    public void recursionFn(List<MenuNode> nodeList, MenuNode node, Integer pId) {
+        List<MenuNode> childList = getChildList(nodeList, node);// 得到子节点列表
+        if (childList.size() > 0) {// 判断是否有子节点
+            if (node.getParentId() == pId) {
+                linkedList.add(node);
+            }
+            Iterator<MenuNode> it = childList.iterator();
+            while (it.hasNext()) {
+                MenuNode n = (MenuNode) it.next();
+                recursionFn(nodeList, n, pId);
+            }
+        } else {
+            if (node.getParentId() == pId) {
+                linkedList.add(node);
+            }
+        }
+    }
+
+    /**
+     * 得到子节点列表
+     *
+     * @author fengshuonan
+     */
+    private List<MenuNode> getChildList(List<MenuNode> list, MenuNode node) {
+        List<MenuNode> nodeList = new ArrayList<MenuNode>();
+        Iterator<MenuNode> it = list.iterator();
+        while (it.hasNext()) {
+            MenuNode n = (MenuNode) it.next();
+            if (n.getParentId() == node.getId()) {
+                nodeList.add(n);
+            }
+        }
+        return nodeList;
+    }
+
+    /**
+     * 清除掉按钮级别的资源
+     *
+     * @date 2017年2月19日 下午11:04:11
+     */
+    public static List<MenuNode> clearBtn(List<MenuNode> nodes) {
+        ArrayList<MenuNode> noBtns = new ArrayList<MenuNode>();
+        for (MenuNode node : nodes) {
+            if (node.getLevels() < 3) {
+                noBtns.add(node);
+            }
+        }
+        return noBtns;
+    }
+
+    /**
+     * 清除不包含子节点的节点
+     *
+     * @date 2017年2月19日 下午11:18:19
+     */
+    public static List<MenuNode> clearNoChild(List<MenuNode> nodes) {
+        ArrayList<MenuNode> results = new ArrayList<MenuNode>();
+        for (MenuNode node : nodes) {
+            if (ToolUtil.isNotEmpty(node.getChildren())) {
+                results.add(node);
+            }
+        }
+        return results;
+    }
+
+    /**
+     * 构建菜单列表
+     *
+     * @date 2017年2月19日 下午11:18:19
+     */
+    public static List<MenuNode> buildTitle(List<MenuNode> nodes) {
+        List<MenuNode> clearBtn = clearBtn(nodes);
+        new MenuNode().buildNodeTree(clearBtn);
+        List<MenuNode> menuNodes = clearNoChild(clearBtn);
+
+        //对菜单排序
+        Collections.sort(menuNodes);
+
+        //对菜单的子菜单进行排序
+        for (MenuNode menuNode : menuNodes) {
+            if (menuNode.getChildren() != null && menuNode.getChildren().size() > 0) {
+                Collections.sort(menuNode.getChildren());
+            }
+        }
+
+        return menuNodes;
+    }
 }
