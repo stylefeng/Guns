a8521dfd329c5dd881ce39671b5db544c1c86c5b
fsn <sn93@qq.com>
2017-05-09 23:54:19 +0800

    增加首页的通知栏

--- src/main/java/com/stylefeng/guns/modular/system/controller/BlackboardController.java
+++ src/main/java/com/stylefeng/guns/modular/system/controller/BlackboardController.java
@@ -1,6 +1,8 @@
 package com.stylefeng.guns.modular.system.controller;
 
 import com.stylefeng.guns.common.controller.BaseController;
+import com.stylefeng.guns.modular.system.dao.NoticeDao;
+import com.stylefeng.guns.persistence.dao.NoticeMapper;
 import com.stylefeng.guns.persistence.dao.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.persistence.model.User;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 总览信息
@@ -22,11 +27,19 @@ public class BlackboardController extends BaseController {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    NoticeMapper noticeMapper;
+
+    @Autowired
+    NoticeDao noticeDao;
+
     /**
      * 跳转到黑板
      */
     @RequestMapping("")
     public String blackboard() {
+        List<Map<String, Object>> notices = noticeDao.list(null);
+        super.setAttr("noticeList",notices);
         super.setAttr("userCount", userMapper.selectCount(new EntityWrapper<User>().notLike("status", "5")));
         super.setAttr("systemCount", super.getSystemInvokCount());
         return "/blackboard.html";
