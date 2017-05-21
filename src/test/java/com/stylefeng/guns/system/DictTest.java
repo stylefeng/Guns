2fae8e2dd4424f01ded94e10564ac2af47e337ef
fsn <sn93@qq.com>
2017-05-21 19:21:21 +0800

    字典的测试完成

--- src/test/java/com/stylefeng/guns/system/DictTest.java
+++ src/test/java/com/stylefeng/guns/system/DictTest.java
@@ -1,10 +1,14 @@
 package com.stylefeng.guns.system;
 
 import com.stylefeng.guns.base.BaseTest;
+import com.stylefeng.guns.modular.system.dao.DictDao;
 import com.stylefeng.guns.modular.system.service.IDictService;
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 字典服务测试
@@ -12,18 +16,34 @@ import javax.annotation.Resource;
  * @author fengshuonan
  * @date 2017-04-27 17:05
  */
-public class DictTest extends BaseTest{
+public class DictTest extends BaseTest {
 
     @Resource
     IDictService dictService;
 
+    @Resource
+    DictDao dictDao;
+
     @Test
     public void addTest() {
-        dictService.addDict("测试123123123","1:冻结;2:jiedong;3:接触");
+        String dictName = "这是一个字典测试";
+        String dictValues = "1:测试1;2:测试2";
+        dictService.addDict(dictName, dictValues);
     }
 
     @Test
     public void editTest() {
-        dictService.editDict(1,"测试","");
+        dictService.editDict(16, "测试", "1:测试1;2:测试2");
+    }
+
+    @Test
+    public void deleteTest() {
+        this.dictService.delteDict(16);
+    }
+
+    @Test
+    public void listTest() {
+        List<Map<String, Object>> list = this.dictDao.list("性别");
+        Assert.assertTrue(list.size() > 0);
     }
 }
