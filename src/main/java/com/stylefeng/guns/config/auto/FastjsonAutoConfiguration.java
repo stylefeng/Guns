6e647ca58ee68527581d84d715512ec3e803a8c6
fsn <sn93@qq.com>
2017-05-23 23:11:57 +0800

    springboot整合fastjson

---
+++ src/main/java/com/stylefeng/guns/config/auto/FastjsonAutoConfiguration.java
@@ -0,0 +1,52 @@
+package com.stylefeng.guns.config.auto;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * fastjson自动配置类
+ *
+ * @author fengshuonan
+ * @date 2017-05-23 22:56
+ */
+@Configuration
+@ConditionalOnClass(FastJsonHttpMessageConverter4.class)
+@ConditionalOnProperty(
+        name = {"spring.http.converters.preferred-json-mapper"},
+        havingValue = "fastjson",
+        matchIfMissing = true
+)
+public class FastjsonAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(FastJsonHttpMessageConverter4.class)
+    public FastJsonHttpMessageConverter4 fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteClassName,
+                SerializerFeature.WriteMapNullValue
+        );
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        ValueFilter valueFilter = new ValueFilter() {
+            public Object process(Object o, String s, Object o1) {
+                if (null == o1) {
+                    o1 = "";
+                }
+                return o1;
+            }
+        };
+        fastJsonConfig.setSerializeFilters(valueFilter);
+        converter.setFastJsonConfig(fastJsonConfig);
+        return converter;
+    }
+
+}
