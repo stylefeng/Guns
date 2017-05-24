fd0f480f004ba34ca47708829febad7ad5b09129
fsn <sn93@qq.com>
2017-05-24 23:43:39 +0800

    头像上传完善

--- src/main/java/com/stylefeng/guns/config/properties/GunsProperties.java
+++ src/main/java/com/stylefeng/guns/config/properties/GunsProperties.java
@@ -3,6 +3,8 @@ package com.stylefeng.guns.config.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+
 import static com.stylefeng.guns.core.util.ToolUtil.getTempPath;
 import static com.stylefeng.guns.core.util.ToolUtil.isEmpty;
 
@@ -22,11 +24,23 @@ public class GunsProperties {
 
     private String fileUploadPath;
 
+    private Boolean haveCreatePath = false;
+
     public String getFileUploadPath() {
-        //如果没有写文件上传路径
+        //如果没有写文件上传路径,保存到临时目录
         if(isEmpty(fileUploadPath)){
             return getTempPath();
         }else{
+            //判断有没有结尾符,没有得加上
+            if(!fileUploadPath.endsWith(File.separator)){
+                fileUploadPath = fileUploadPath + File.separator;
+            }
+            //判断目录存不存在,不存在得加上
+            if(haveCreatePath == false){
+                File file = new File(fileUploadPath);
+                file.mkdirs();
+                haveCreatePath = true;
+            }
             return fileUploadPath;
         }
     }
