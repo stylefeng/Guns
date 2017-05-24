91e4eabc9045f79cc492d0d225b8863b29426c9b
fsn <sn93@qq.com>
2017-05-24 23:46:57 +0800

    完善fastjson配置

--- src/main/java/com/stylefeng/guns/config/FastjsonConfig.java
+++ src/main/java/com/stylefeng/guns/config/FastjsonConfig.java
@@ -1,32 +1,22 @@
-package com.stylefeng.guns.config.auto;
+package com.stylefeng.guns.config;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * fastjson自动配置类
+ * fastjson配置类
  *
  * @author fengshuonan
  * @date 2017-05-23 22:56
  */
 @Configuration
-@ConditionalOnClass(FastJsonHttpMessageConverter4.class)
-@ConditionalOnProperty(
-        name = {"spring.http.converters.preferred-json-mapper"},
-        havingValue = "fastjson",
-        matchIfMissing = true
-)
-public class FastjsonAutoConfiguration {
+public class FastjsonConfig {
 
     @Bean
-    @ConditionalOnMissingBean(FastJsonHttpMessageConverter4.class)
     public FastJsonHttpMessageConverter4 fastJsonHttpMessageConverter() {
         FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
