5d9653bb882499f2ea6db7a19dadf164b184d8de
naan1993 <sn93@qq.com>
2017-07-11 23:17:11 +0800

    修复拖出菜单中的页面之后home图标不起作用的bug

--- src/main/webapp/static/js/content.js
+++ src/main/webapp/static/js/content.js
@@ -37,7 +37,7 @@ $('.close-link').click(function () {
 
 //判断当前页面是否在iframe中
 if (top == this) {
-    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="index.html?v=4.0" title="返回首页"><i class="fa fa-home"></i></a></div>';
+    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="' + Feng.ctxPath + '/" title="返回首页"><i class="fa fa-home"></i></a></div>';
     $('body').append(gohome);
 }
 
