0eaa537d4661892945970b5f892a1ed868e7f687
olunx <olunx@qq.com>
2017-08-16 11:04:23 +0800

    添加多数据源类拦截

--- src/main/java/com/stylefeng/guns/common/annotion/DataSource.java
+++ src/main/java/com/stylefeng/guns/common/annotion/DataSource.java
@@ -1,6 +1,10 @@
 package com.stylefeng.guns.common.annotion;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 
@@ -11,7 +15,7 @@ import java.lang.annotation.*;
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD })
+@Target({ ElementType.METHOD,ElementType.TYPE})
 public @interface DataSource {
 
 	String name() default "";
