c7200d9fd7ec16ef656898947fadab4eb1505f62
naan1993 <sn93@qq.com>
2017-09-05 21:42:36 +0800

    更新测试

--- guns-rest/src/test/java/com/stylefeng/guns/jwt/DecryptTest.java
+++ guns-rest/src/test/java/com/stylefeng/guns/jwt/DecryptTest.java
@@ -1,6 +1,9 @@
 package com.stylefeng.guns.jwt;
 
-import io.jsonwebtoken.Jwts;
+import com.alibaba.fastjson.JSON;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.SimpleObject;
+import com.stylefeng.guns.rest.modular.auth.converter.BaseTransferEntity;
 
 /**
  * jwt测试
@@ -14,10 +17,21 @@ public class DecryptTest {
 
         String key = "mySecret";
 
-        String compactJws = "eyJhbGciOiJIUzUxMiJ9.eyJyYW5kb21LZXkiOiJudjBsd3QiLCJzdWIiOiJhZG1pbiIsImV4cCI6MTUwNDI0NzA1NSwiaWF0IjoxNTAzNjQyMjU1fQ.wHzVxTvi0bmfq8YmI65tVqYfeXp5EJPzm5C_DtQOl5Fyc1HKuDJyaW-BPpjgMtjsk-mdeEBZSadoDK3LjHsy8A";
+        String compactJws = "eyJhbGciOiJIUzUxMiJ9.eyJyYW5kb21LZXkiOiJ0dDA5emciLCJzdWIiOiJhZG1pbiIsImV4cCI6MTUwNTIyMjU1MiwiaWF0IjoxNTA0NjE3NzUyfQ.wFn1U3qBDZNDlPOkTxOnsbn8U1qjMveyqvbARviJ1tOQ_giFhbToIup4r-Xvy0AaiFnGt2YFB25MA-YFXGDl9Q";
+        String salt = "tt09zg";
 
-        System.out.println("body = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody());
-        System.out.println("header = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getHeader());
-        System.out.println("signature = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getSignature());
+        SimpleObject simpleObject = new SimpleObject();
+        simpleObject.setUser("stylefeng");
+        String md5 = MD5Util.encrypt(JSON.toJSONString(simpleObject) + salt);
+
+        BaseTransferEntity baseTransferEntity = new BaseTransferEntity();
+        baseTransferEntity.setObject(simpleObject);
+        baseTransferEntity.setSign(md5);
+
+        System.out.println(JSON.toJSON(baseTransferEntity));
+
+        //System.out.println("body = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody());
+        //System.out.println("header = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getHeader());
+        //System.out.println("signature = " + Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getSignature());
     }
 }
