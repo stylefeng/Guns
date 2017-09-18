aedaabd13b3cef83bf1dfe72d16dbb0e1f36bf99
naan1993 <sn93@qq.com>
2017-09-18 21:12:41 +0800

    修改注释

--- guns-rest/src/main/java/com/stylefeng/guns/rest/modular/auth/security/DataSecurityAction.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/modular/auth/security/DataSecurityAction.java
@@ -1,7 +1,12 @@
 package com.stylefeng.guns.rest.modular.auth.security;
 
 /**
+ * <pre>
  * 信息传递的保护措施(传递的数据为json)
+ *
+ * 说明:
+ * 可以根据实际开发时的需要,编写自己的数据加密方案,只需实现此类,并在WebConfig下配置您所编写的实现类即可
+ * </pre>
  *
  * @author fengshuonan
  * @date 2017-09-18 20:41
@@ -9,7 +14,7 @@ package com.stylefeng.guns.rest.modular.auth.security;
 public interface DataSecurityAction {
 
     /**
-     * 执行数据的保护措施(可以实现自定义的保护措施)
+     * 执行数据的保护措施
      *
      * @author stylefeng
      * @Date 2017/9/18 20:42
