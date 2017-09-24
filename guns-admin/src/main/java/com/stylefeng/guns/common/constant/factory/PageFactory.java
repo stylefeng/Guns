dfaac43671d194719c9c29f243079eda57337fb5
naan1993 <sn93@qq.com>
2017-09-24 21:52:16 +0800

    完善注释

--- guns-admin/src/main/java/com/stylefeng/guns/common/constant/factory/PageFactory.java
+++ guns-admin/src/main/java/com/stylefeng/guns/common/constant/factory/PageFactory.java
@@ -17,19 +17,19 @@ public class PageFactory<T> {
 
     public Page<T> defaultPage() {
         HttpServletRequest request = HttpKit.getRequest();
-        int limit = Integer.valueOf(request.getParameter("limit"));
-        int offset = Integer.valueOf(request.getParameter("offset"));
-        String sort = request.getParameter("sort");
-        String order = request.getParameter("order");
-        if(ToolUtil.isEmpty(sort)){
+        int limit = Integer.valueOf(request.getParameter("limit"));     //每页多少条数据
+        int offset = Integer.valueOf(request.getParameter("offset"));   //每页的偏移量(本页当前有多少条)
+        String sort = request.getParameter("sort");         //排序字段名称
+        String order = request.getParameter("order");       //asc或desc(升序或降序)
+        if (ToolUtil.isEmpty(sort)) {
             Page<T> page = new Page<>((offset / limit + 1), limit);
             page.setOpenSort(false);
             return page;
-        }else{
+        } else {
             Page<T> page = new Page<>((offset / limit + 1), limit, sort);
-            if(Order.ASC.getDes().equals(order)){
+            if (Order.ASC.getDes().equals(order)) {
                 page.setAsc(true);
-            }else{
+            } else {
                 page.setAsc(false);
             }
             return page;
