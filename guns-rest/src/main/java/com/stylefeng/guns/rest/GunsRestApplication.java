601f0fcde2618392765f90f8fd5a0b14903af75d
stylefeng <sn93@qq.com>
2017-09-29 09:02:52 +0800

    增加rest模块包扫描 core模块

--- guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
@@ -3,7 +3,7 @@ package com.stylefeng.guns.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 public class GunsRestApplication {
 
     public static void main(String[] args) {
