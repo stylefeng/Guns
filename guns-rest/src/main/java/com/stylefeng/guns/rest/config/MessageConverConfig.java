0fd09505ee0640fa4da476e182bf74597ce94588
naan1993 <sn93@qq.com>
2017-10-23 21:12:26 +0800

    删除没用的bean

--- guns-rest/src/main/java/com/stylefeng/guns/rest/config/MessageConverConfig.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/config/MessageConverConfig.java
@@ -1,6 +1,5 @@
 package com.stylefeng.guns.rest.config;
 
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
 import com.stylefeng.guns.core.config.DefaultFastjsonConfig;
 import com.stylefeng.guns.rest.config.properties.RestProperties;
 import com.stylefeng.guns.rest.modular.auth.converter.WithSignMessageConverter;
@@ -25,15 +24,5 @@ public class MessageConverConfig {
         withSignMessageConverter.setFastJsonConfig(defaultFastjsonConfig.fastjsonConfig());
         withSignMessageConverter.setSupportedMediaTypes(defaultFastjsonConfig.getSupportedMediaType());
         return withSignMessageConverter;
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = RestProperties.REST_PREFIX, name = "sign-open", havingValue = "false")
-    public FastJsonHttpMessageConverter4 fastJsonHttpMessageConverter4() {
-        FastJsonHttpMessageConverter4 FastJsonHttpMessageConverter4 = new FastJsonHttpMessageConverter4();
-        DefaultFastjsonConfig defaultFastjsonConfig = new DefaultFastjsonConfig();
-        FastJsonHttpMessageConverter4.setFastJsonConfig(defaultFastjsonConfig.fastjsonConfig());
-        FastJsonHttpMessageConverter4.setSupportedMediaTypes(defaultFastjsonConfig.getSupportedMediaType());
-        return FastJsonHttpMessageConverter4;
     }
 }
