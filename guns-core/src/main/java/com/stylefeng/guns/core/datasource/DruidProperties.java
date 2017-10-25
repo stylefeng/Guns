ac488b1eecc8cd5930aff9fa27541ce227259e76
stylefeng <sn93@qq.com>
2017-10-25 17:29:23 +0800

    修改druid配置类位置

--- guns-core/src/main/java/com/stylefeng/guns/core/datasource/DruidProperties.java
+++ guns-core/src/main/java/com/stylefeng/guns/core/datasource/DruidProperties.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 /**
  * <p>数据库数据源配置</p>
  * <p>说明:这个类中包含了许多默认配置,若这些配置符合您的情况,您可以不用管,若不符合,建议不要修改本类,建议直接在"application.yml"中配置即可</p>
- * @author fengshuonan
+ * @author stylefeng
  * @date 2017-05-21 11:18
  */
 @Component
