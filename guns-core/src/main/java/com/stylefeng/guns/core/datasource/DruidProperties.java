559499977553222ae3f02086aff1010887201bb0
冯硕楠 <shuonan.feng@lanmaoly.com>
2017-10-25 17:26:04 +0800

    修改druid配置类位置

--- guns-core/src/main/java/com/stylefeng/guns/core/datasource/DruidProperties.java
+++ guns-core/src/main/java/com/stylefeng/guns/core/datasource/DruidProperties.java
@@ -1,4 +1,4 @@
-package com.stylefeng.guns.config.properties;
+package com.stylefeng.guns.core.datasource;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
