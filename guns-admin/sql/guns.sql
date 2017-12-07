90d6f9cb7fcbcfde4336470eb2d8278db778385a
naan1993 <sn93@qq.com>
2017-12-07 23:35:06 +0800

    更新sql语句 增加添加和删除数据库的语句

--- guns-admin/sql/guns.sql
+++ guns-admin/sql/guns.sql
@@ -13,6 +13,14 @@ File Encoding         : 65001
 Date: 2017-12-05 22:56:40
 */
 
+DROP DATABASE IF EXISTS guns_flowable;
+CREATE DATABASE guns_flowable;
+
+DROP DATABASE IF EXISTS guns;
+CREATE DATABASE guns;
+
+USE guns;
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
