0ae277b9fb811194f76163149d09179b8108a07c
fengshuonan <sn93@qq.com>
2018-07-10 11:17:17 +0800

    修复启动错误的bug

--- guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
@@ -3,7 +3,7 @@ package com.stylefeng.guns.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
+@SpringBootApplication
 public class GunsRestApplication {
 
     public static void main(String[] args) {
