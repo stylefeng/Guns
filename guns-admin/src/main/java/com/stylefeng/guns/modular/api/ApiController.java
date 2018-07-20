3d5d3f7c9492b13de0ad91bb6730187a9633737d
stylefeng <sn93@qq.com>
2018-07-20 23:47:35 +0800

    测试鉴权接口

---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/api/ApiController.java
@@ -0,0 +1,34 @@
+package com.stylefeng.guns.modular.api;
+
+import com.stylefeng.guns.core.base.controller.BaseController;
+import com.stylefeng.guns.core.util.JwtTokenUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 接口控制器提供
+ *
+ * @author stylefeng
+ * @Date 2018/7/20 23:39
+ */
+@RestController
+@RequestMapping("/gunsapi")
+public class ApiController extends BaseController {
+
+    /**
+     * 测试接口是否走鉴权
+     */
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public Object test() {
+        return SUCCESS_TIP;
+    }
+
+    /**
+     * 模拟生成一个token
+     */
+    public static void main(String[] args) {
+        System.out.println(JwtTokenUtil.generateToken("aaa", null));
+    }
+}
+
