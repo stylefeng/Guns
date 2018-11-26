b8a0fd83b63b4fe117be25008239468cb66e4486
fengshuonan <sn93@qq.com>
2018-11-26 10:01:36 +0800

    修复资源映射问题

--- src/main/java/cn/stylefeng/guns/config/web/WebConfig.java
+++ src/main/java/cn/stylefeng/guns/config/web/WebConfig.java
@@ -60,13 +60,18 @@ public class WebConfig implements WebMvcConfigurer {
     private GunsProperties gunsProperties;
 
     /**
-     * 增加swagger的支持
+     * 静态资源映射
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if (gunsProperties.getSwaggerOpen()) {
+
+            //swagger
             registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
             registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+            //本应用
+            registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
         }
     }
 
