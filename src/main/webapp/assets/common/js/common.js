7225ca344a50254c19bc0aea88730463062ea260
fengshuonan <sn93@qq.com>
2019-02-06 22:26:18 +0800

    修复ctxPath问题

--- src/main/webapp/assets/common/js/common.js
+++ src/main/webapp/assets/common/js/common.js
@@ -1,94 +1,87 @@
-var Feng = {
-    ctxPath: "",
-    addCtx: function (ctx) {
-        if (this.ctxPath === "") {
-            this.ctxPath = ctx;
-        }
-    },
-    info: function (info) {
-        top.layer.msg(info, {icon: 6});
-    },
-    success: function (info) {
-        top.layer.msg(info, {icon: 1});
-    },
-    error: function (info) {
-        top.layer.msg(info, {icon: 2});
-    },
-    confirm: function(tip, ensure){
-        top.layer.confirm(tip, {
-            skin: 'layui-layer-admin'
-        }, function () {
-            ensure();
-        });
-    },
-    currentDate: function () {
-        // 获取当前日期
-        var date = new Date();
+// 用common.js必须加上Feng.addCtx("${ctxPath}");
+Feng.info = function (info) {
+    top.layer.msg(info, {icon: 6});
+};
+Feng.success = function (info) {
+    top.layer.msg(info, {icon: 1});
+};
+Feng.error = function (info) {
+    top.layer.msg(info, {icon: 2});
+};
+Feng.confirm = function (tip, ensure) {
+    top.layer.confirm(tip, {
+        skin: 'layui-layer-admin'
+    }, function () {
+        ensure();
+    });
+};
+Feng.currentDate = function () {
+    // 获取当前日期
+    var date = new Date();
 
-        // 获取当前月份
-        var nowMonth = date.getMonth() + 1;
+    // 获取当前月份
+    var nowMonth = date.getMonth() + 1;
 
-        // 获取当前是几号
-        var strDate = date.getDate();
+    // 获取当前是几号
+    var strDate = date.getDate();
 
-        // 添加分隔符“-”
-        var seperator = "-";
+    // 添加分隔符“-”
+    var seperator = "-";
 
-        // 对月份进行处理，1-9月在前面添加一个“0”
-        if (nowMonth >= 1 && nowMonth <= 9) {
-            nowMonth = "0" + nowMonth;
-        }
-
-        // 对月份进行处理，1-9号在前面添加一个“0”
-        if (strDate >= 0 && strDate <= 9) {
-            strDate = "0" + strDate;
-        }
-
-        // 最后拼接字符串，得到一个格式为(yyyy-MM-dd)的日期
-        return date.getFullYear() + seperator + nowMonth + seperator + strDate;
-    },
-    getUrlParam: function (name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) {
-            return unescape(r[2]);
-        } else {
-            return null;
-        }
-    },
-    infoDetail: function (title, info) {
-        var display = "";
-        if (typeof info === "string") {
-            display = info;
-        } else {
-            if (info instanceof Array) {
-                for (var x in info) {
-                    display = display + info[x] + "<br/>";
-                }
-            } else {
-                display = info;
-            }
-        }
-        top.layer.open({
-            title: title,
-            type: 1,
-            skin: 'layui-layer-rim', //加上边框
-            area: ['950px', '600px'], //宽高
-            content: '<div style="padding: 20px;">' + display + '</div>'
-        });
-    },
-    zTreeCheckedNodes: function (zTreeId) {
-        var zTree = $.fn.zTree.getZTreeObj(zTreeId);
-        var nodes = zTree.getCheckedNodes();
-        var ids = "";
-        for (var i = 0, l = nodes.length; i < l; i++) {
-            ids += "," + nodes[i].id;
-        }
-        return ids.substring(1);
-    },
-    closeAllLoading: function () {
-        layer.closeAll('loading');
+    // 对月份进行处理，1-9月在前面添加一个“0”
+    if (nowMonth >= 1 && nowMonth <= 9) {
+        nowMonth = "0" + nowMonth;
     }
+
+    // 对月份进行处理，1-9号在前面添加一个“0”
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+
+    // 最后拼接字符串，得到一个格式为(yyyy-MM-dd)的日期
+    return date.getFullYear() + seperator + nowMonth + seperator + strDate;
+};
+Feng.getUrlParam = function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    } else {
+        return null;
+    }
+};
+Feng.infoDetail = function (title, info) {
+    var display = "";
+    if (typeof info === "string") {
+        display = info;
+    } else {
+        if (info instanceof Array) {
+            for (var x in info) {
+                display = display + info[x] + "<br/>";
+            }
+        } else {
+            display = info;
+        }
+    }
+    top.layer.open({
+        title: title,
+        type: 1,
+        skin: 'layui-layer-rim', //加上边框
+        area: ['950px', '600px'], //宽高
+        content: '<div style="padding: 20px;">' + display + '</div>'
+    });
+};
+Feng.zTreeCheckedNodes = function (zTreeId) {
+    var zTree = $.fn.zTree.getZTreeObj(zTreeId);
+    var nodes = zTree.getCheckedNodes();
+    var ids = "";
+    for (var i = 0, l = nodes.length; i < l; i++) {
+        ids += "," + nodes[i].id;
+    }
+    return ids.substring(1);
+};
+Feng.closeAllLoading = function () {
+    layer.closeAll('loading');
 };
 
 // 以下代码是配置layui扩展模块的目录，每个页面都需要引入
