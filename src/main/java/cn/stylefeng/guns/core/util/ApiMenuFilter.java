0794581ee2c855658b7d0d5af4b26515893fb336
zoain-it <1.2.zoain@gmail.com>
2019-03-29 15:25:50 +0800

    修复关闭swagger后还是显示接口文档的问题

--- src/main/java/cn/stylefeng/guns/core/util/ApiMenuFilter.java
+++ src/main/java/cn/stylefeng/guns/core/util/ApiMenuFilter.java
@@ -20,6 +20,7 @@ import cn.stylefeng.guns.core.common.constant.Const;
 import cn.stylefeng.guns.core.common.node.MenuNode;
 import cn.stylefeng.roses.core.util.SpringContextHolder;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,22 +32,29 @@ import java.util.List;
  */
 public class ApiMenuFilter extends MenuNode {
 
-    public static List<MenuNode> build(List<MenuNode> nodes) {
-
+        public static List<MenuNode> build(List<MenuNode> nodes) {
         //如果关闭了接口文档,则不显示接口文档菜单
-        GunsProperties gunsProperties = SpringContextHolder.getBean(GunsProperties.class);
-        if (!gunsProperties.getSwaggerOpen()) {
-            List<MenuNode> menuNodesCopy = new ArrayList<>();
-            for (MenuNode menuNode : nodes) {
-                if (Const.API_MENU_NAME.equals(menuNode.getName())) {
-                    continue;
-                } else {
-                    menuNodesCopy.add(menuNode);
-                }
-            }
-            nodes = menuNodesCopy;
+        ManageProperties manageProperties = SpringContextHolder.getBean(ManageProperties.class);
+        if (!manageProperties.getSwaggerOpen()) {
+            nodes = filterMenuByName(nodes, Const.API_MENU_NAME);
         }
-
         return nodes;
     }
+
+    private static List<MenuNode> filterMenuByName(List<MenuNode> nodes, @NotNull String menuName) {
+        List<MenuNode> menuNodesCopy = new ArrayList<>(nodes.size());
+        for (MenuNode menuNode : nodes) {
+            if (menuName.equals(menuNode.getName())) {
+                continue;
+            } else {
+                menuNodesCopy.add(menuNode);
+            }
+            List<MenuNode> childrenList = menuNode.getChildren();
+            if (childrenList != null && childrenList.size() > 0) {
+                menuNode.setChildren(filterMenuByName(childrenList, menuName));
+            }
+        }
+        return menuNodesCopy;
+    }
+
 }
