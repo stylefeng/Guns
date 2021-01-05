5600e36f6a1338fda6bc243c6db072c9df524530
TSQ <1970742763@qq.com>
2021-01-05 16:00:13 +0800

    登陆日志控制器

---
+++ src/main/java/cn/stylefeng/guns/modular/log/LoginLogController.java
@@ -0,0 +1,36 @@
+package cn.stylefeng.guns.modular.log;
+
+import cn.stylefeng.roses.kernel.resource.api.annotation.ApiResource;
+import cn.stylefeng.roses.kernel.resource.api.annotation.GetResource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 登陆日志管理控制器
+ *
+ * @param
+ * @return
+ * @author TSQ
+ * @date 2021/1/5 14:42
+ */
+@Controller
+@Slf4j
+@ApiResource(name = "登陆日志管理相关的界面渲染", path = "loginLog")
+public class LoginLogController {
+
+    private String PREFIX = "/modular/system/log";
+
+    /**
+     * 登陆日志管理列表
+     * 
+     * @param 
+     * @return 
+     * @author TSQ
+     * @date 2021/1/5 15:17
+     */
+    @GetResource(name="登陆日志管理列表" , path = "", requiredPermission = false ,requiredLogin = false)
+    public String indexView(){
+        return PREFIX + "login_log.html";
+    }
+
+}
