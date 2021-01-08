5bdd0ab2d2d29e00404d240967c7e97b659c9e17
fengshuonan <sn93@qq.com>
2021-01-08 18:33:38 +0800

    整理修改菜单界面

--- src/main/webapp/assets/modular/auth/menu/menu_edit.js
+++ src/main/webapp/assets/modular/auth/menu/menu_edit.js
@@ -3,59 +3,59 @@
  */
 var MenuInfoDlg = {
     data: {
-        pid: "",
-        pcodeName: ""
+        menuParentId: "",
+        menuParentName: ""
     }
 };
 
-layui.use(['layer', 'form', 'admin', 'laydate', 'ax', 'iconPicker'], function () {
+layui.use(['layer', 'form', 'admin', 'laydate', 'HttpRequest', 'iconPicker'], function () {
     var $ = layui.jquery;
-    var $ax = layui.ax;
+    var HttpRequest = layui.HttpRequest;
     var form = layui.form;
     var admin = layui.admin;
     var laydate = layui.laydate;
     var layer = layui.layer;
     var iconPicker = layui.iconPicker;
 
-    //初始化字典详细列表
+    //初始化所属应用列表
     $("#dictDetails").html('');
-    var ajax = new $ax(Feng.ctxPath + "/dict/listDictsByCode", function (data) {
+    var appListRequest = new HttpRequest(Feng.ctxPath + "/sysApp/list", 'get', function (data) {
         for (var i = 0; i < data.data.length; i++) {
-            var name = data.data[i].name;
-            var code = data.data[i].code;
-            $("#dictDetails").append('<input type="radio" name="systemType" value="' + code + '" title="' + name + '">');
+            var name = data.data[i].appName;
+            var code = data.data[i].appCode;
+            $("#dictDetails").append('<input type="radio" name="appCode" value="' + code + '" title="' + name + '">');
         }
+        form.render();
     }, function (data) {
     });
-    ajax.set("dictTypeCode", "SYSTEM_TYPE");
-    ajax.start();
+    appListRequest.start();
 
     //获取菜单信息
-    var ajax = new $ax(Feng.ctxPath + "/menu/getMenuInfo?menuId=" + Feng.getUrlParam("menuId"));
-    var result = ajax.start();
-    form.val('menuForm', result.data);
+    var request = new HttpRequest(Feng.ctxPath + "/sysMenu/detail?menuId=" + Feng.getUrlParam("menuId"), 'get');
+    var menuInfoResult = request.start();
+    form.val('menuForm', menuInfoResult.data);
 
     // 点击父级菜单
-    $('#pcodeName').click(function () {
-        var formName = encodeURIComponent("parent.MenuInfoDlg.data.pcodeName");
-        var formId = encodeURIComponent("parent.MenuInfoDlg.data.pid");
-        var treeUrl = encodeURIComponent("/menu/selectMenuTreeList");
+    $('#menuParentName').click(function () {
+        var formName = encodeURIComponent("parent.MenuInfoDlg.data.menuParentName");
+        var formId = encodeURIComponent("parent.MenuInfoDlg.data.menuParentId");
+        var treeUrl = encodeURIComponent("/sysMenu/layuiSelectParentMenuTreeList");
 
         layer.open({
             type: 2,
             title: '父级菜单',
             area: ['300px', '400px'],
-            content: Feng.ctxPath + '/system/commonTree?formName=' + formName + "&formId=" + formId + "&treeUrl=" + treeUrl,
+            content: Feng.ctxPath + '/view/common/tree?formName=' + formName + "&formId=" + formId + "&treeUrl=" + treeUrl,
             end: function () {
-                $("#pid").val(MenuInfoDlg.data.pid);
-                $("#pcodeName").val(MenuInfoDlg.data.pcodeName);
+                $("#menuParentId").val(MenuInfoDlg.data.menuParentId);
+                $("#menuParentName").val(MenuInfoDlg.data.menuParentName);
             }
         });
     });
 
     // 表单提交事件
     form.on('submit(btnSubmit)', function (data) {
-        var ajax = new $ax(Feng.ctxPath + "/menu/edit", function (data) {
+        var request = new HttpRequest(Feng.ctxPath + "/sysMenu/edit", 'post', function (data) {
             Feng.success("修改成功！");
 
             //传给上个页面，刷新table用
@@ -65,10 +65,10 @@ layui.use(['layer', 'form', 'admin', 'laydate', 'ax', 'iconPicker'], function ()
             admin.closeThisDialog();
 
         }, function (data) {
-            Feng.error("修改失败！" + data.responseJSON.message)
+            Feng.error("修改失败！" + data.message)
         });
-        ajax.set(data.field);
-        ajax.start();
+        request.set(data.field);
+        request.start(true);
 
         //添加 return false 可成功跳转页面
         return false;
@@ -76,15 +76,14 @@ layui.use(['layer', 'form', 'admin', 'laydate', 'ax', 'iconPicker'], function ()
 
     //初始化图标选择
     iconPicker.render({
-        elem: '#icon',
+        elem: '#layuiIcon',
         type: 'fontClass',
         search: true,
         page: true,
         limit: 12,
         click: function (data) {
-
         }
     });
 
-    iconPicker.checkIcon('iconPicker', result.data.icon);
+    iconPicker.checkIcon('iconPicker', menuInfoResult.data.layuiIcon);
 });
