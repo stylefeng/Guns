44e42fa764b2235221c338a74e2f5b2e3422fc00
fengshuonan <sn93@qq.com>
2021-01-13 13:17:59 +0800

    更新代码

--- src/main/java/cn/stylefeng/guns/modular/dict/DictTypeViewController.java
+++ src/main/java/cn/stylefeng/guns/modular/dict/DictTypeViewController.java
@@ -9,53 +9,45 @@ import org.springframework.stereotype.Controller;
 /**
  * 字典类型视图控制器
  *
- * @Author: huangyao
- * @Date: 2021/1/6 20:53
+ * @author huangyao
+ * @date 2021/1/6 20:53
  **/
 @Controller
 @Slf4j
 @ApiResource(name = "字典类型管理相关的界面渲染")
 public class DictTypeViewController {
 
-    private String PREFIX = "/modular/system/dictType";
-
     /**
      * 字典类型管理-字典类型-视图
      *
-     * @param
-     * @return
      * @author huangyao
      * @date 2021/1/6 21:08
      */
     @GetResource(name = "字典类型管理-列表-视图", path = "/view/dictType")
     public String indexView() {
-        return PREFIX + "/dictType.html";
+        return "/modular/system/dictType/dictType.html";
     }
 
     /**
      * 字典类型管理-添加-视图
      *
-     * @param
-     * @return
      * @author huangyao
      * @date 2021/1/6 21:25
      */
     @GetResource(name = "字典类型管理-添加-视图", path = "/view/dictType/addView")
     public String addView() {
-        return PREFIX + "/dictType_add.html";
+        return "/modular/system/dictType/dictType_add.html";
     }
 
     /**
      * 字典类型管理-编辑-视图
      *
-     * @param
-     * @return
      * @author huangyao
      * @date 2021/1/6 21:26
      */
     @GetResource(name = "字典类型管理-编辑-视图", path = "/view/dictType/editView")
     public String editView() {
-        return PREFIX + "/dictType_edit.html";
+        return "/modular/system/dictType/dictType_edit.html";
     }
 
 }
--- src/main/java/cn/stylefeng/guns/modular/file/FileViewController.java
+++ src/main/java/cn/stylefeng/guns/modular/file/FileViewController.java
@@ -1,4 +1,4 @@
-package cn.stylefeng.guns.modular.file.controller;
+package cn.stylefeng.guns.modular.file;
 
 import cn.stylefeng.roses.kernel.resource.api.annotation.ApiResource;
 import cn.stylefeng.roses.kernel.resource.api.annotation.GetResource;
@@ -7,18 +7,24 @@ import org.springframework.stereotype.Controller;
 
 /**
  * 文件管理界面
- * @author : lgq
- * @date : 2021/1/9
+ *
+ * @author lgq
+ * @date 2021/1/9
  */
 @Controller
 @Slf4j
 @ApiResource(name = "文件管理界面")
 public class FileViewController {
 
-    @GetResource(name = "菜单管理首页", path = "/view/file")
+    /**
+     * 文件管理首页
+     *
+     * @author lgq
+     * @date 2021/1/9
+     */
+    @GetResource(name = "文件管理首页", path = "/view/file")
     public String fileIndex() {
         return "/modular/system/fileInfo/file_info.html";
     }
-
 
 }
