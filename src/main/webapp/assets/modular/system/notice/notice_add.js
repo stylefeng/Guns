d7d3d64f57bcac9df15e986e824040f9c33aee2b
liuhanqing <447067298@qq.com>
2021-01-14 21:49:57 +0800

    【notice】人员选择开发
    Signed-off-by: liuhanqing <447067298@qq.com>

--- src/main/webapp/assets/modular/system/notice/notice_add.js
+++ src/main/webapp/assets/modular/system/notice/notice_add.js
@@ -1,16 +1,17 @@
-layui.use(['layer', 'form', 'admin', 'laydate', 'HttpRequest'], function () {
+layui.use(['layer', 'form', 'admin', 'laydate', 'HttpRequest', 'xmSelect'], function () {
     var $ = layui.jquery;
     var HttpRequest = layui.HttpRequest;
     var form = layui.form;
     var admin = layui.admin;
     var layer = layui.layer;
     var laydate = layui.laydate;
+    var xmSelect = layui.xmSelect;
 
 
-    lay('input.date-input').each(function(){
+    lay('input.date-input').each(function () {
         laydate.render({
             elem: this
-            ,trigger: 'click'
+            , trigger: 'click'
             , type: 'datetime'
         });
     });
@@ -34,31 +35,39 @@ layui.use(['layer', 'form', 'admin', 'laydate', 'HttpRequest'], function () {
         }
     });
 
-/*
-    //实例化编辑器
-    var ue = UE.getEditor('container', {
-        enableAutoSave: false,
-        autoHeightEnabled: true,
-        autoFloatEnabled: false,
-        scaleEnabled: true,         //滚动条
-        initialFrameHeight: 400     //默认的编辑区域高度
-    });
+    /*
+        //实例化编辑器
+        var ue = UE.getEditor('container', {
+            enableAutoSave: false,
+            autoHeightEnabled: true,
+            autoFloatEnabled: false,
+            scaleEnabled: true,         //滚动条
+            initialFrameHeight: 400     //默认的编辑区域高度
+        });
 
-    UE.Editor.prototype._bkGetActionUrl = UE.Editor.prototype.getActionUrl;
-    UE.Editor.prototype.getActionUrl = function (action) {
-        if (action === 'uploadimage' || action === 'uploadscrawl' || action === 'uploadimage') {
-            return Feng.ctxPath + '/ueditor/imgUpdate';
-        } else if (action === 'uploadfile') {
-            return Feng.ctxPath + '/ueditor/uploadfile';
-        } else if (action === 'uploadvideo') {
-            return Feng.ctxPath + '/ueditor/uploadvideo';
-        } else {
-            return this._bkGetActionUrl.call(this, action);
-        }
-    };*/
+        UE.Editor.prototype._bkGetActionUrl = UE.Editor.prototype.getActionUrl;
+        UE.Editor.prototype.getActionUrl = function (action) {
+            if (action === 'uploadimage' || action === 'uploadscrawl' || action === 'uploadimage') {
+                return Feng.ctxPath + '/ueditor/imgUpdate';
+            } else if (action === 'uploadfile') {
+                return Feng.ctxPath + '/ueditor/uploadfile';
+            } else if (action === 'uploadvideo') {
+                return Feng.ctxPath + '/ueditor/uploadvideo';
+            } else {
+                return this._bkGetActionUrl.call(this, action);
+            }
+        };*/
+
+    $("#test").click(function (){
+        var select = userSelect.getValue('valueStr');
+        alert(select)
+        console.log(select)
+        return;
+    })
 
     // 表单提交事件
     form.on('submit(btnSubmit)', function (data) {
+
         var request = new HttpRequest(Feng.ctxPath + "/sysNotice/add", 'post', function (data) {
             admin.closeThisDialog();
             Feng.success("添加成功！");
@@ -73,5 +82,64 @@ layui.use(['layer', 'form', 'admin', 'laydate', 'HttpRequest'], function () {
         //添加 return false 可成功跳转页面
         return false;
     });
+
+
+    var userSelect = xmSelect.render({
+        el: '#userSelect',
+        autoRow: true,
+        filterable: true,
+        filterMethod: function (val, item, index, prop) {
+
+            if (item.name.indexOf(val) != -1) {//名称中包含的搜索出来
+                return true;
+            }
+            // 添加拼音检索
+            try {
+                if (Pinyin.GetJP(item.name).indexOf(val) != -1 || Pinyin.GetQP(item.name).indexOf(val) != -1) {
+                    return true
+                }
+            } catch (e) {
+                console.log(e.message);
+            }
+            return false;//不知道的就不管了
+        },
+        tree: {
+            show: true,
+            showFolderIcon: true,
+            showLine: true,
+            indent: 20,
+            expandedKeys: true,
+        },
+        toolbar: {
+            show: true,
+            list: ['ALL', 'REVERSE', 'CLEAR']
+        },
+        height: 'auto',
+        data: function () {
+            return [
+                {
+                    name: '销售员', value: -1, disabled: true, children: [
+                        {name: '张三1', value: 1, selected: true, children: []},
+                        {name: '李四1', value: 2, selected: true},
+                        {name: '王五1', value: 3, disabled: true},
+                    ]
+                },
+                {
+                    name: '奖品', value: -2, children: [
+                        {
+                            name: '奖品3', value: -3, children: [
+                                {name: '苹果3', value: 14, selected: true},
+                                {name: '香蕉3', value: 15},
+                                {name: '葡萄3', value: 16},
+                            ]
+                        },
+                        {name: '苹果2', value: 4, selected: true, disabled: true},
+                        {name: '香蕉2', value: 5},
+                        {name: '葡萄2', value: 6},
+                    ]
+                },
+            ]
+        }
+    })
 
 });
