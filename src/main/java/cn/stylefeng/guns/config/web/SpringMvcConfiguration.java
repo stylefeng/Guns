34c0e511b7413a991c5dfaa4a89f97309d012848
fengshuonan <sn93@qq.com>
2021-01-16 21:49:14 +0800

    新增资源映射

--- src/main/java/cn/stylefeng/guns/config/web/SpringMvcConfiguration.java
+++ src/main/java/cn/stylefeng/guns/config/web/SpringMvcConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -30,18 +31,6 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
 
     @Resource
     private PermissionSecurityInterceptor permissionSecurityInterceptor;
-
-    /**
-     * 配置项目拦截器
-     *
-     * @author fengshuonan
-     * @date 2020/12/18 9:43
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authJwtTokenSecurityInterceptor);
-        registry.addInterceptor(permissionSecurityInterceptor);
-    }
 
     /**
      * 重写系统的默认错误提示
@@ -77,6 +66,29 @@ public class SpringMvcConfiguration implements WebMvcConfigurer {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance).serializerByType(Long.TYPE, ToStringSerializer.instance);
+    }
+
+    /**
+     * 配置项目拦截器
+     *
+     * @author fengshuonan
+     * @date 2020/12/18 9:43
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authJwtTokenSecurityInterceptor);
+        registry.addInterceptor(permissionSecurityInterceptor);
+    }
+
+    /**
+     * 静态资源映射
+     *
+     * @author fengshuonan
+     * @date 2021/1/16 21:45
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
     }
 
 }
