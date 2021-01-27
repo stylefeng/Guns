f4da698d5431e87bb01979df4a15aec34a379fdc
fengshuonan <sn93@qq.com>
2021-01-27 23:01:39 +0800

    更新html的多语言翻译

--- src/main/java/cn/stylefeng/guns/core/beetl/CustomBeetlGroupUtilConfiguration.java
+++ src/main/java/cn/stylefeng/guns/core/beetl/CustomBeetlGroupUtilConfiguration.java
@@ -5,6 +5,7 @@ import cn.stylefeng.guns.core.beetl.tag.SysDictCheckBoxTag;
 import cn.stylefeng.guns.core.beetl.tag.SysDictRadioTag;
 import cn.stylefeng.guns.core.beetl.tag.SysDictSelectTag;
 import cn.stylefeng.roses.kernel.auth.api.LoginUserApi;
+import cn.stylefeng.roses.kernel.i18n.util.QuickTranslateUtil;
 import cn.stylefeng.roses.kernel.system.expander.SystemConfigExpander;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 
@@ -34,6 +35,9 @@ public class CustomBeetlGroupUtilConfiguration extends BeetlGroupUtilConfigurati
         // 获取基本信息的工具
         groupTemplate.registerFunctionPackage("constants", SystemConfigExpander.class);
 
+        // 多语言
+        groupTemplate.registerFunctionPackage("translate", QuickTranslateUtil.class);
+
         // 下拉选字典
         groupTemplate.registerTag("dict_select", SysDictSelectTag.class);
 
@@ -42,9 +46,6 @@ public class CustomBeetlGroupUtilConfiguration extends BeetlGroupUtilConfigurati
 
         // 多选字典
         groupTemplate.registerTag("dict_checkbox", SysDictCheckBoxTag.class);
-
-        // todo 多语言
-//        groupTemplate.registerFunctionPackage("lang", new UserTranslationContext());
     }
 
 }
