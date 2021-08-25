bf4440c595d15dd1b49c4ec062664c450eb92cf1
fengshuonan <sn93@qq.com>
2021-08-25 16:06:51 +0800

    【7.1.2】更新应用图标

---
+++ src/main/resources/db/migration/mysql/V7.1.2_20210825_1601__app_update_icon.sql
@@ -0,0 +1,1 @@
+UPDATE `sys_app` SET `app_name` = '系统应用', `app_code` = 'system', `app_icon` = 'SettingTwoTone', `active_flag` = 'Y', `status_flag` = 1, `del_flag` = 'N', `create_time` = '2020-03-25 19:07:00', `create_user` = 1265476890672672808, `update_time` = '2021-08-25 16:00:07', `update_user` = 1339550467939639299 WHERE `app_id` = 1265476890672672821;
