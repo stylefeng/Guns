a5243646dfd8db525f67f0fc890caf1378767e30
fengshuonan <sn93@qq.com>
2022-04-28 21:25:47 +0800

    默认设置所有菜单都是前后台都显示，可以自行调整

--- src/main/resources/db/migration/mysql/V7.2.2_20220428_1143__update_menu_front_type.sql
+++ src/main/resources/db/migration/mysql/V7.2.2_20220428_1143__update_menu_front_type.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `sys_menu` ADD COLUMN `antdv_front_type` tinyint(4) NULL DEFAULT NULL COMMENT '前台还是后台菜单：1-前台，2-后台，3-前后台都显示' AFTER `antdv_visible`;
+
+update sys_menu set antdv_front_type = 3;
