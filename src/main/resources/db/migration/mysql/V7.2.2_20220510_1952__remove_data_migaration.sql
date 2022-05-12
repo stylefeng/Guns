27fcf3ccb49081798a47cc238cd64683471b991d
fengshuonan <sn93@qq.com>
2022-05-12 15:53:59 +0800

    【7.2.2】删除migration菜单

---
+++ src/main/resources/db/migration/mysql/V7.2.2_20220510_1952__remove_data_migaration.sql
@@ -0,0 +1,7 @@
+delete from sys_menu where menu_id in ('1412617944147038209','1412618684643020802');
+
+delete from sys_role_menu where role_menu_id = '1425335779983577089';
+
+delete from sys_role_menu where role_menu_id = '1425335838166962177';
+
+delete from sys_menu_resource where menu_resource_id in (1425350033721733122, 1425350033763676161, 1425350033780453377);
