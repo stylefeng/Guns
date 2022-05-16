6ba8438469ef2ebeeb70c7d7e37293587e1096c9
fengshuonan <sn93@qq.com>
2022-05-17 00:12:11 +0800

    【7.2.2】更新单点配置的分组

---
+++ src/main/resources/db/migration/mysql/V7.2.2_20220517_0031__update_auth_ca_config.sql
@@ -0,0 +1,9 @@
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610205;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610210;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610215;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610220;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610225;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610230;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610235;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610240;
+UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610245;
