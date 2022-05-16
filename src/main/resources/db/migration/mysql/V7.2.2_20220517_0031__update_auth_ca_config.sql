40e835c12f0b31133619a0d5637913c30ae526a3
fengshuonan <sn93@qq.com>
2022-05-17 00:12:55 +0800

    【7.2.2】更新配置分组

--- src/main/resources/db/migration/mysql/V7.2.2_20220517_0031__update_auth_ca_config.sql
+++ src/main/resources/db/migration/mysql/V7.2.2_20220517_0031__update_auth_ca_config.sql
@@ -1,3 +1,5 @@
+INSERT INTO `sys_dict`(`dict_id`, `dict_code`, `dict_name`, `dict_name_pinyin`, `dict_encode`, `dict_type_code`, `dict_short_name`, `dict_short_code`, `dict_parent_id`, `status_flag`, `dict_sort`, `dict_pids`, `del_flag`, `create_time`, `create_user`, `update_time`, `update_user`) VALUES (1526221204984197121, 'ca_config', '单点配置', 'ddpz', NULL, 'config_group', NULL, NULL, -1, 1, 70.00, '[-1],', 'N', '2022-05-16 23:21:06', 1339550467939639299, NULL, NULL);
+
 UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610205;
 UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610210;
 UPDATE `sys_config` SET `group_code` = 'ca_config' WHERE `config_id` = 1402549781675610215;
