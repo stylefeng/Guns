20b1b0aa268faf5d65acbf012ed0216da9510bb7
fengshuonan <sn93@qq.com>
2022-06-20 18:44:15 +0800

    【7.2.3】添加角色绑定资源的各个界面

---
+++ src/main/resources/db/migration/mysql/V7.2.3_20220620_1843__add_role_bind_page.sql
@@ -0,0 +1,5 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+INSERT INTO `sys_menu`(`menu_id`, `menu_parent_id`, `menu_pids`, `menu_name`, `menu_code`, `app_code`, `menu_sort`, `status_flag`, `remark`, `layui_path`, `layui_icon`, `layui_visible`, `antdv_router`, `antdv_icon`, `antdv_component`, `antdv_link_open_type`, `antdv_link_url`, `antdv_active_url`, `antdv_visible`, `antdv_front_type`, `del_flag`, `create_time`, `create_user`, `update_time`, `update_user`) VALUES (1538775070967664642, 1399362846198013953, '[-1],[1399362846198013953],', '用户权限', 'role_power', 'systemApp', 20.70, 1, NULL, NULL, NULL, 'Y', '/authority/userPower', 'UserOutlined', '/system/user-power/index', 0, NULL, NULL, 'Y', 1, 'N', '2022-06-20 14:45:41', 1339550467939639299, NULL, NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
