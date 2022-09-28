0a65379336f681a2dd5594deb44c5cfa3e5cb846
fengshuonan <sn93@qq.com>
2022-09-28 16:23:37 +0800

    【7.2.5】删除系统菜单的业务类型

--- src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
+++ src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
@@ -1,5 +1,1 @@
-ALTER TABLE `sys_menu` ADD COLUMN `menu_biz_type` tinyint(4) NULL DEFAULT 2 COMMENT '菜单的业务类型：1-系统类型，2-业务类型' AFTER `remark`;
-
-UPDATE `sys_menu` SET menu_biz_type = 1;
-
 ALTER TABLE `sys_resource` ADD COLUMN `resource_biz_type` tinyint NULL DEFAULT 2 COMMENT '资源的业务类型：1-系统类，2-业务类' AFTER `http_method`;
