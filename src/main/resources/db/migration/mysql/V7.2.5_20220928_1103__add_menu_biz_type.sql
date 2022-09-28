03f4b0d60b5e788d6f5edf78632c80e982ab4c78
fengshuonan <sn93@qq.com>
2022-09-28 23:44:41 +0800

    【7.2.5】修改资源类型字段

--- src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
+++ src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `sys_resource` ADD COLUMN `resource_biz_type` tinyint NULL DEFAULT 2 COMMENT '资源的业务类型：1-系统类，2-业务类' AFTER `http_method`;
+ALTER TABLE `sys_resource` ADD COLUMN `resource_biz_type` tinyint NULL DEFAULT 1 COMMENT '资源的业务类型：1-业务类，2-系统类' AFTER `http_method`;
