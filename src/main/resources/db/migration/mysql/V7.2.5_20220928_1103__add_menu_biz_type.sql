c9e72f926e27a8a4f09c958af2b822f5101f9d55
fengshuonan <sn93@qq.com>
2022-09-28 11:31:12 +0800

    【7.2.5】更新菜单类型默认为系统类型

--- src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
+++ src/main/resources/db/migration/mysql/V7.2.5_20220928_1103__add_menu_biz_type.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `sys_menu` ADD COLUMN `menu_biz_type` tinyint NULL COMMENT '菜单的业务类型：1-系统类型，2-业务类型' AFTER `remark`;
+ALTER TABLE `sys_menu` ADD COLUMN `menu_biz_type` tinyint(4) NULL DEFAULT 2 COMMENT '菜单的业务类型：1-系统类型，2-业务类型' AFTER `remark`;
+
+UPDATE `sys_menu` SET menu_biz_type = 1;
