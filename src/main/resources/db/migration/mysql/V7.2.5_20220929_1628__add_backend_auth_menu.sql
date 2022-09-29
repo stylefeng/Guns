1c9e37a36b290569931a0b625611c8bfc6734522
fengshuonan <sn93@qq.com>
2022-09-29 21:24:45 +0800

    【7.2.5】整理sql

--- src/main/resources/db/migration/mysql/V7.2.5_20220929_1628__add_backend_auth_menu.sql
+++ src/main/resources/db/migration/mysql/V7.2.5_20220929_1628__add_backend_auth_menu.sql
@@ -2,45 +2,13 @@ INSERT INTO `sys_menu`(`menu_id`, `menu_parent_id`, `menu_pids`, `menu_name`, `m
 
 UPDATE `sys_menu` SET `menu_name` = '前台权限' WHERE `menu_id` = 1538775070967664642;
 
+UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `app_code` = 'systemApp';
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639301;
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639302;
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639303;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639304;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639305;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639307;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639309;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639310;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639311;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639312;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639313;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639314;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639315;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639317;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639318;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639319;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639320;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639321;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639334;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639335;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639336;
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639350;
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639351;
 UPDATE `sys_menu` SET `antdv_front_type` = 1 WHERE `menu_id` = 1339550467939639352;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639360;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639361;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639362;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1339550467939639390;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1399362846198013953;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1399366927557795841;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1472812560315629569;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1472814801600692226;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1472815202244804610;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1475271836221173761;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1508982886328541185;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1508983390106394626;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1508983647087206402;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1508984172092432385;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1527832618693947394;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1527852532272062466;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1538775070967664642;
-UPDATE `sys_menu` SET `antdv_front_type` = 2 WHERE `menu_id` = 1575396079602528258;
+
+-- 个人中心前后台都显示
+UPDATE `sys_menu` SET `antdv_front_type` = 3 WHERE `menu_id` = 1339550467939639390;
