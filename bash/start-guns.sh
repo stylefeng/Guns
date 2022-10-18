729aa7b811e1ad1ecae897d389fe1c0ee93ffac4
fengshuonan <sn93@qq.com>
2022-10-18 20:35:42 +0800

    【7.2.5】更新启动脚本内容

--- bash/start-guns.sh
+++ bash/start-guns.sh
@@ -6,9 +6,6 @@ do
   echo "Kill the $1 process [ $i ]"
   kill -9 $i
 done
-echo "  =====启动Java应用======"
+echo "  =====关闭Guns应用======"
 nohup java -Xms1024M -Xmx1024M -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=512M -jar guns.jar --spring.profiles.active=local > guns.out &
-echo $! > guns-pid.txt
-cat guns-pid.txt
-rm  guns-pid.txt
 tail -f guns.out
