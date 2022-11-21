a59f97d0cdab7f4880a31674782605422333f974
fengshuonan <sn93@qq.com>
2022-11-22 00:04:27 +0800

    【7.3.3】更新消息跳转的url字段

--- src/main/resources/db/migration/mysql/V7.3.3_20221121_2307__add_notice_type.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `sys_notice`
-    ADD COLUMN `notice_type` tinyint NULL DEFAULT 1 COMMENT '通知类型：1-普通通知，2-带url跳转链接' AFTER `notice_content`,
-    ADD COLUMN `notice_url` varchar(500) NULL COMMENT '通知的完整跳转url' AFTER `notice_type`;
