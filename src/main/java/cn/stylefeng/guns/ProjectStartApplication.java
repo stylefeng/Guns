9d9bdabd9adea163114fa9cbff3d13bf2d8d2974
fengshuonan <sn93@qq.com>
2023-06-21 15:58:22 +0800

    【7.6.0】临时打印一些id

--- src/main/java/cn/stylefeng/guns/ProjectStartApplication.java
+++ src/main/java/cn/stylefeng/guns/ProjectStartApplication.java
@@ -1,6 +1,7 @@
 package cn.stylefeng.guns;
 
 import cn.stylefeng.roses.kernel.db.starter.SingleDataSourceAutoConfiguration;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +14,25 @@ import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
  * @since 2020/12/1 17:50
  */
 @Slf4j
-@SpringBootApplication(scanBasePackages = {"cn.stylefeng"}, exclude = {FlywayAutoConfiguration.class, SingleDataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"cn.stylefeng"},
+        exclude = {FlywayAutoConfiguration.class, SingleDataSourceAutoConfiguration.class})
 public class ProjectStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectStartApplication.class, args);
         log.info(ProjectStartApplication.class.getSimpleName() + " is success!");
+
+        // 等上线去掉
+        while (true) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(IdWorker.getId());
+        }
+
     }
 
 }
