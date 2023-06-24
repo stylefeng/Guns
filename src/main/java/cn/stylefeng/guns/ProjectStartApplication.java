387dea763fcbcea95cae780c964bd5aa5c076745
fengshuonan <sn93@qq.com>
2023-06-25 00:07:46 +0800

    【7.6.0】更新依赖

--- src/main/java/cn/stylefeng/guns/ProjectStartApplication.java
+++ src/main/java/cn/stylefeng/guns/ProjectStartApplication.java
@@ -1,7 +1,6 @@
 package cn.stylefeng.guns;
 
 import cn.stylefeng.roses.kernel.db.starter.SingleDataSourceAutoConfiguration;
-import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
